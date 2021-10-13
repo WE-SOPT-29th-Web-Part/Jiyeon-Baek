2c4877ae96751551bb90f572c516a06592775c73
100Gyeon <bjy9966@naver.com>
2021-10-13 17:00:03 +0900

    feat: 모달창 구현

--- velog-clone/main.js
+++ velog-clone/main.js
@@ -3,6 +3,12 @@
 const navPeriod = document.querySelector(".nav__period");
 const navDropdown = document.querySelector(".nav__dropdown");
 const navText = document.querySelector(".nav__text");
+const items = document.querySelectorAll(".item");
+const body = document.querySelector("body");
+const modalBackground = document.querySelector(".modal__background");
+const modalWrapper = document.querySelector(".modal__wrapper");
+const modalContent = document.querySelector(".modal__content");
+const closeBtn = document.querySelector(".modal__close");
 
 navPeriod.addEventListener("click", () => {
   if (navDropdown.style.visibility == "hidden") {
@@ -19,4 +25,22 @@ navDropdown.addEventListener("click", (e) => {
     element.classList.remove("active")
   );
   e.target.classList.add("active");
+});
+
+items.forEach(item => {
+  item.addEventListener("click", () => {
+    body.style.overflow = "hidden";
+    modalBackground.classList.add("show");
+    modalWrapper.classList.add("show");
+
+    const clone = item.cloneNode(true);
+    modalContent.appendChild(clone);
+
+    closeBtn.addEventListener("click", () => {
+      body.style.overflow = "scroll";
+      modalBackground.classList.remove("show");
+      modalWrapper.classList.remove("show");
+      clone.remove();
+    });
+  });
 });
