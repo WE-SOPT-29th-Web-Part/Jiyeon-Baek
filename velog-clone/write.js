c8a958a6b17e4e942ce90c07466929bd5fc03327
100Gyeon <bjy9966@naver.com>
2021-10-13 20:54:23 +0900

    feat: 텍스트 중복 입력 제한 기능 추가

--- velog-clone/write.js
+++ velog-clone/write.js
@@ -1,16 +1,20 @@
 'use strict';
 
 const tagInput = document.querySelector(".tag-input");
+let filterArr = [];
 
-tagInput.addEventListener("keyup", (event) => {
-  if (event.key === "Enter") {
-    addTag(tagInput.value);
+tagInput.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    if (filterArr.includes(e.target.value) || !e.target.value) {
+      e.target.value = "";
+      return;
+    }
+    filterArr.push(e.target.value);
+    addTag(e.target.value);
   }
 });
 
 const addTag = (text) => {
-  if (!text) return;
-
   const span = document.createElement("span");
   span.setAttribute("class", "tag-span");
   span.innerText = text;
