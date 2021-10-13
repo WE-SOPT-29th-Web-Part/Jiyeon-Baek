41625cf1e8ac9f26eb0fe15778167cd386f37dda
100Gyeon <bjy9966@naver.com>
2021-10-13 18:17:14 +0900

    chore: 불필요한 변수 제거

--- velog-clone/main.js
+++ velog-clone/main.js
@@ -4,7 +4,6 @@ const navPeriod = document.querySelector(".nav__period");
 const navDropdown = document.querySelector(".nav__dropdown");
 const navText = document.querySelector(".nav__text");
 const items = document.querySelectorAll(".item");
-const body = document.querySelector("body");
 const modalBackground = document.querySelector(".modal__background");
 const modalWrapper = document.querySelector(".modal__wrapper");
 const modalContent = document.querySelector(".modal__content");
@@ -29,7 +28,7 @@ navDropdown.addEventListener("click", (e) => {
 
 items.forEach(item => {
   item.addEventListener("click", () => {
-    body.style.overflow = "hidden";
+    document.body.style.overflow = "hidden";
     modalBackground.classList.add("show");
     modalWrapper.classList.add("show");
 
@@ -37,7 +36,7 @@ items.forEach(item => {
     modalContent.appendChild(clone);
 
     closeBtn.addEventListener("click", () => {
-      body.style.overflow = "scroll";
+      document.body.style.overflow = "scroll";
       modalBackground.classList.remove("show");
       modalWrapper.classList.remove("show");
       clone.remove();
