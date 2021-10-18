532551836121f6e31fd1fdc84ef45dc745a6b233
100Gyeon <bjy9966@naver.com>
2021-10-18 19:09:58 +0900

    feat: 다크 모드 기능 구현

--- velog-clone/src/main.js
+++ velog-clone/src/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const checkbox = document.querySelector(".header__checkbox");
 const navPeriod = document.querySelector(".nav__period");
 const navDropdown = document.querySelector(".nav__dropdown");
 const navText = document.querySelector(".nav__text");
@@ -9,6 +10,29 @@ const modalWrapper = document.querySelector(".modal__wrapper");
 const modalContent = document.querySelector(".modal__content");
 const closeBtn = document.querySelector(".modal__close");
 
+// 다크모드 구현
+document.documentElement.setAttribute("color-theme", "light"); // 기본값을 light로 설정
+
+checkbox.addEventListener("change", (e) => {
+  // setItem을 통해 로컬 스토리지에 아이템 저장
+  if (e.target.checked) {
+    localStorage.setItem("color-theme", "dark");
+    document.documentElement.setAttribute("color-theme", "dark");
+  } else {
+    localStorage.setItem("color-theme", "light");
+    document.documentElement.setAttribute("color-theme", "light");
+  }
+});
+
+const currentTheme = localStorage.getItem("color-theme"); // getItem을 통해 로컬 스토리지에서 아이템의 값(테마) 알아내기
+if (currentTheme) { // 로컬 스토리지에 아이템이 있으면
+  document.documentElement.setAttribute("color-theme", currentTheme); // 그 아이템의 값으로 color-theme 설정
+  if (currentTheme === "dark") { // 다크모드면 checked 속성을 true로 설정
+    checkbox.checked = true;
+  }
+}
+
+// 커스텀 드롭다운 구현 
 navPeriod.addEventListener("click", () => {
   if (navDropdown.style.visibility === "visible") {
     navDropdown.style.visibility = "hidden";
@@ -26,9 +50,10 @@ navDropdown.addEventListener("click", (e) => {
   e.target.classList.add("active");
 });
 
+// 모달창 구현
 items.forEach(item => {
   item.addEventListener("click", () => {
-    document.body.style.overflow = "hidden";
+    document.body.style.overflow = "hidden"; // body 스크롤 방지
     modalBackground.classList.add("show");
     modalWrapper.classList.add("show");
 
