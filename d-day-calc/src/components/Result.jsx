c69ee6c656d090a32d97347ef279664e44742701
100Gyeon <bjy9966@naver.com>
2021-10-25 20:55:41 +0900

    chore: 불필요한 부분 삭제

--- d-day-calc/src/components/Result.jsx
+++ d-day-calc/src/components/Result.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Text1 from './Text1';
 import Text2 from './Text2';
 
-const Result = ({ year, month, date, setInput }) => {
+const Result = ({ year, month, date }) => {
   return (
     <div id="result">
-      <Text1 year={year} month={month} date={date} onChange={setInput} />
+      <Text1 year={year} month={month} date={date} />
       <Text2 year={year} month={month} date={date} />
     </div>
   );
