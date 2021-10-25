21f3cac531e08fe529d1c3f75a3152311e8d02bd
100Gyeon <bjy9966@naver.com>
2021-10-25 23:12:10 +0900

    style: prettier 적용

--- d-day-calc/src/components/DateInput.jsx
+++ d-day-calc/src/components/DateInput.jsx
@@ -6,20 +6,38 @@ const DateInput = ({ year, month, date, setYear, setMonth, setDate }) => {
     setYear(today.getFullYear());
     setMonth(today.getMonth() + 1);
     setDate(today.getDate());
-  }
-  
+  };
+
   // input 값이 변화하면 그 값을 year, month, date로 바꿔준다.
   const handleChange = (e, setState) => {
     setState(e.target.value);
-  }
+  };
 
   return (
     <div id="dateInput">
       <button onClick={setToday}>오늘</button>
       <div>
-        <input onChange={(e) => handleChange(e, setYear)} type="text" className="dateInput__input" value={year} />년
-        <input onChange={(e) => handleChange(e, setMonth)} type="text" className="dateInput__input" value={month} />월
-        <input onChange={(e) => handleChange(e, setDate)} type="text" className="dateInput__input" value={date} />일을 기준으로
+        <input
+          onChange={(e) => handleChange(e, setYear)}
+          type="text"
+          className="dateInput__input"
+          value={year}
+        />
+        년
+        <input
+          onChange={(e) => handleChange(e, setMonth)}
+          type="text"
+          className="dateInput__input"
+          value={month}
+        />
+        월
+        <input
+          onChange={(e) => handleChange(e, setDate)}
+          type="text"
+          className="dateInput__input"
+          value={date}
+        />
+        일을 기준으로
       </div>
     </div>
   );
--- d-day-calc/src/components/Text2.jsx
+++ d-day-calc/src/components/Text2.jsx
@@ -1,34 +1,38 @@
 import React, { useCallback, useState, useEffect } from 'react';
 
 const Text2 = ({ year, month, date }) => {
-  const [inputValue, setInputValue] = useState("");
-  const [resultDate, setResultDate] = useState("yyyy년 mm월 dd일");
+  const [inputValue, setInputValue] = useState('');
+  const [resultDate, setResultDate] = useState('yyyy년 mm월 dd일');
 
   const handleChange = (e) => {
     if (!e.target.value) {
-      setInputValue("");
-      setResultDate("yyyy년 mm월 dd일");
+      setInputValue('');
+      setResultDate('yyyy년 mm월 dd일');
       return;
     }
     setInputValue(e.target.value);
     printDate(e.target.value);
-  }
+  };
 
-  const printDate = useCallback((value) => {
-    if(value === "") {
-      setResultDate("yyyy년 mm월 dd일");
-      return;
-    }
-    
-    const tempDate = new Date();
+  const printDate = useCallback(
+    (value) => {
+      if (value === '') {
+        setResultDate('yyyy년 mm월 dd일');
+        return;
+      }
 
-    tempDate.setFullYear(year);
-    tempDate.setMonth(month - 1);
-    tempDate.setDate(Number(date) - Number(value));
+      const tempDate = new Date();
+      tempDate.setFullYear(year);
+      tempDate.setMonth(month - 1);
+      tempDate.setDate(Number(date) - Number(value));
 
-    const result = `${tempDate.getFullYear()}년 ${tempDate.getMonth() + 1}월 ${tempDate.getDate()}일`;
-    setResultDate(result);
-  }, [year, month, date]);
+      const result = `${tempDate.getFullYear()}년 ${
+        tempDate.getMonth() + 1
+      }월 ${tempDate.getDate()}일`;
+      setResultDate(result);
+    },
+    [year, month, date],
+  );
 
   useEffect(() => {
     printDate(inputValue);
@@ -38,7 +42,8 @@ const Text2 = ({ year, month, date }) => {
     <div className="text">
       <div>
         D-
-        <input type="text" value={inputValue} onChange={handleChange} />는?
+        <input type="text" value={inputValue} onChange={handleChange} />
+        는?
       </div>
       <div>{resultDate}</div>
     </div>
