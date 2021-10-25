9f19c3b429a55b636f09da15d8de5313b46d134a
100Gyeon <bjy9966@naver.com>
2021-10-25 20:51:42 +0900

    feat: 기준일로부터 디데이 계산하는 기능 구현

--- d-day-calc/src/components/Text2.jsx
+++ d-day-calc/src/components/Text2.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Text2 = ({ year, month, date }) => {
+  const [inputValue, setInputValue] = useState("");
+  const [resultDate, setResultDate] = useState("yyyy년 mm월 dd일");
+
+  const handleChange = (e) => {
+    if (!e.target.value) {
+      setInputValue("");
+      setResultDate("yyyy년 mm월 dd일");
+      return;
+    }
+    setInputValue(e.target.value);
+    printDate(e.target.value);
+  }
+
+  const printDate = (value) => {
+    const tempDate = new Date();
+
+    tempDate.setFullYear(year);
+    tempDate.setMonth(month - 1);
+    tempDate.setDate(Number(date) - Number(value));
+
+    const result = `${tempDate.getFullYear()}년 ${tempDate.getMonth() + 1}월 ${tempDate.getDate()}일`;
+    setResultDate(result);
+  };
+
   return (
     <div className="text">
       <div>
         D-
-        <input type="text"/>는?
+        <input type="text" value={inputValue} onChange={handleChange} />는?
       </div>
-      <div>yyyy년 mm월 dd일</div>
+      <div>{resultDate}</div>
     </div>
   );
 };
