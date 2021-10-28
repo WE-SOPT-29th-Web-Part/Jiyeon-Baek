5186bbdcf29584b802a4b3ea8f78a3c3034e82ee
100Gyeon <bjy9966@naver.com>
2021-10-28 18:13:02 +0900

    feat: 검색하면 API 데이터를 받아서 card를 보여주는 기능, 닫기 버튼 구현

--- github-profile-finder/src/App.jsx
+++ github-profile-finder/src/App.jsx
@@ -1,19 +1,33 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import Result from './components/Result';
+import styled from 'styled-components';
 
 function App() {
-  const [userInfo, setUserInfo] = useState();
+  const [userInfo, setUserInfo] = useState({});
+
+  useEffect(() => {
+    console.log(`userInfo: `, userInfo);
+  }, [userInfo]);
 
   return (
-    <div id="app">
+    <Root>
       <Header />
-      <SearchBar />
-      <Result />
-    </div>
+      <SearchBar setUserInfo={setUserInfo} />
+      <Result userInfo={userInfo} setUserInfo={setUserInfo} />
+    </Root>
   );
 }
 
 export default App;
+
+const Root = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
--- github-profile-finder/src/components/SearchBar.jsx
+++ github-profile-finder/src/components/SearchBar.jsx
@@ -1,8 +1,49 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
 
-const SearchBar = () => {
+const SearchBar = ({ setUserInfo }) => {
   const [user, setUser] = useState();
-  return <div></div>;
+  const handleChange = (e) => {
+    setUser(e.target.value);
+  };
+
+  // 값을 submit 했을 때, 검색이 되도록.
+  // input 태그를 form 태그로 감싸고, onSubmit 이벤트를 사용
+
+  const handleSubmit = async (e) => {
+    // submit 하면 새로고침 되는 것을 방지
+    e.preventDefault();
+
+    // user 값을 이용하여 서버에 있는 데이터 받아오기
+    // 서버 통신에 시간 소요 -> 비동기 처리 (async, await)
+    // axios는 서버 통신을 돕는 툴
+    const { data } = await axios.get(`https://api.github.com/users/${user}`);
+    setUserInfo(data);
+    setUser('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Input
+        value={user}
+        onChange={handleChange}
+        type="text"
+        placeholder="Github 프로필을 검색해 보세요."
+      />
+    </form>
+  );
 };
 
 export default SearchBar;
+
+const Input = styled.input`
+  width: 320px;
+  height: 57px;
+  padding: 16px;
+  color: rgb(229, 230, 231);
+  background-color: rgb(36, 39, 43);
+  outline: none;
+  border: 8px solid rgb(105, 105, 105, 0.5);
+  border-radius: 20px;
+`;
