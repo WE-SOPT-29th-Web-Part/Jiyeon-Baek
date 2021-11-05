1163d9c1d4cd09ca385ed87692e4fbcf00a410fe
100Gyeon <bjy9966@naver.com>
2021-11-05 16:27:19 +0900

    feat: 로딩 애니메이션 구현

--- github-profile-finder/src/components/PendingResult.jsx
+++ github-profile-finder/src/components/PendingResult.jsx
@@ -2,12 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 
 const PendingResult = () => {
-  return <Root>Loading...</Root>;
+  return <Root></Root>;
 };
 
 export default PendingResult;
 
 const Root = styled.div`
   margin-top: 16px;
-  color: white;
+  border: 8px solid #f3f3f3;
+  border-top: 8px solid gold;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 2s linear infinite;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(3600deg);
+    }
+  }
 `;
