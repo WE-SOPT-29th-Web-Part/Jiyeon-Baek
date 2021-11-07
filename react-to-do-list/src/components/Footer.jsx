4db1aa3e12cf6680f6ea2f719527aa8212c8f870
100Gyeon <bjy9966@naver.com>
2021-11-07 16:14:48 +0900

    feat: Header, Footer 컴포넌트 생성 및 스타일링

---
+++ react-to-do-list/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Footer = () => {
+  return <StyledFooter>SOPT 29th WEB Part</StyledFooter>;
+};
+
+export default Footer;
+
+const StyledFooter = styled.footer`
+  color: white;
+  background-color: thistle;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+`;
