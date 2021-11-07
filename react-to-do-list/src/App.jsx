b3498bb47dad27bb0a4dd047660cd8956c1b319b
100Gyeon <bjy9966@naver.com>
2021-11-08 02:48:20 +0900

    fix: 화면 100% 채우도록 수정

--- react-to-do-list/src/App.jsx
+++ react-to-do-list/src/App.jsx
@@ -1,8 +1,8 @@
-import "./App.css";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Navigation from "./components/Navigation";
-import TodosContainer from "./components/TodosContainer";
+import './App.css';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import TodosContainer from './components/TodosContainer';
+import Footer from './components/Footer';
 
 function App() {
   return (
