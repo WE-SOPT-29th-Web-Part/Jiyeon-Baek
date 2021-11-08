4a6aafcb59a0708440ccd01d56f37d1dd6e776eb
100Gyeon <bjy9966@naver.com>
2021-11-08 16:44:07 +0900

    style: 프리티어 적용

--- react-to-do-list/src/components/Footer.jsx
+++ react-to-do-list/src/components/Footer.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 const Footer = () => {
   return <StyledFooter>SOPT 29th WEB Part</StyledFooter>;
