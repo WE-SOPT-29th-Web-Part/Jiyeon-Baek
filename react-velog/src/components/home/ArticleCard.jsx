f739fd78376ae042b0697c6f3a62011924f4e732
100Gyeon <bjy9966@naver.com>
2021-11-09 05:54:23 +0900

    feat: ArticleCard 스타일링

--- react-velog/src/components/home/ArticleCard.jsx
+++ react-velog/src/components/home/ArticleCard.jsx
@@ -1,18 +1,73 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const ArticleCard = ({ article }) => {
   const { id, title, body, summary, series, tags, thumbnail, date } = article;
   return (
-    <div>
+    <StyledCard>
       <img src={thumbnail} alt="" />
       <h3>{title}</h3>
       <h4>{summary}</h4>
-      {tags.map((tag) => (
-        <span key={tag}>{tag}</span>
-      ))}
+      <StyledTag>
+        {tags.map((tag) => (
+          <span key={tag}>{tag}</span>
+        ))}
+      </StyledTag>
       <div>{date}</div>
-    </div>
+    </StyledCard>
   );
 };
 
 export default ArticleCard;
+
+const StyledCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 4rem 0;
+  line-height: 1.5;
+  border-bottom: 1px solid rgb(233, 236, 239);
+  width: 768px;
+  margin: 0 auto;
+
+  & > img {
+    object-fit: cover;
+    height: 20rem;
+  }
+
+  & > h3 {
+    font-size: 1.5rem;
+    font-weight: 600;
+    margin-top: 1rem;
+    color: rgb(33, 37, 41);
+    word-break: keep-all;
+  }
+
+  & > h4 {
+    margin-bottom: 2rem;
+    margin-top: 0.5rem;
+    font-size: 1rem;
+    color: rgb(73, 80, 87);
+    word-break: keep-all;
+    overflow-wrap: break-word;
+  }
+
+  & > div {
+    color: rgb(134, 142, 150);
+    font-size: 0.875rem;
+  }
+`;
+
+const StyledTag = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  & > span {
+    background: rgb(241, 243, 245);
+    color: rgb(12, 166, 120);
+    padding: 0 1rem;
+    height: 2rem;
+    line-height: 2rem;
+    font-size: 1rem;
+    border-radius: 1rem;
+    margin-right: 0.875rem;
+  }
+`;
