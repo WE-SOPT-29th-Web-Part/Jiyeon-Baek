deb57406496e2b8c260902f479b8a2d6f96a0bcd
100Gyeon <bjy9966@naver.com>
2021-11-09 05:54:11 +0900

    feat: Header 스타일링

--- react-velog/src/components/common/Header.jsx
+++ react-velog/src/components/common/Header.jsx
@@ -1,22 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { ReactComponent as VelogIcon } from '../../assets/icons/velog.svg';
 
 const Header = () => {
   return (
-    <div>
-      <StyledLeft>sopt.log</StyledLeft>
+    <StyledHeader>
+      <StyledLeft>
+        <VelogIcon width="30" height="30" />
+        <div>JiYeon.log</div>
+      </StyledLeft>
       <StyledRight>
         <Link to="/write">
           <button>새 글 작성</button>
         </Link>
       </StyledRight>
-    </div>
+    </StyledHeader>
   );
 };
 
 export default Header;
 
-const StyledLeft = styled.div``;
+const StyledHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 5rem;
+`;
 
-const StyledRight = styled.div``;
+const StyledLeft = styled.div`
+  display: flex;
+  align-items: center;
+
+  & > div {
+    margin-left: 1rem;
+    letter-spacing: 0.1rem;
+    font-weight: 600;
+    font-size: 1.2rem;
+  }
+`;
+
+const StyledRight = styled.div`
+  & button {
+    border-radius: 15px;
+    background-color: white;
+    border: 1px solid black;
+    padding: 5px 10px;
+  }
+`;
