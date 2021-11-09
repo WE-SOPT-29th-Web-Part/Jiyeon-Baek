648b2a0168d13695f763a212a15814bbf53e949b
100Gyeon <bjy9966@naver.com>
2021-11-09 22:51:23 +0900

    feat: Header 스타일링

--- react-velog/src/components/common/Header.jsx
+++ react-velog/src/components/common/Header.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { ReactComponent as VelogIcon } from '../../assets/icons/velog.svg';
+import { ReactComponent as SearchIcon } from '../../assets/icons/search.svg';
+import { ReactComponent as MoreIcon } from '../../assets/icons/more.svg';
+import profileImg from '../../assets/images/profile.png';
 
 const Header = () => {
   return (
@@ -11,9 +14,12 @@ const Header = () => {
         <div>JiYeon.log</div>
       </StyledLeft>
       <StyledRight>
+        <SearchIcon />
         <Link to="/write">
           <button>새 글 작성</button>
         </Link>
+        <img src={profileImg} alt="profile" />
+        <MoreIcon />
       </StyledRight>
     </StyledHeader>
   );
@@ -41,15 +47,41 @@ const StyledLeft = styled.div`
 `;
 
 const StyledRight = styled.div`
+  display: flex;
+  align-items: center;
+
+  & svg:first-child {
+    margin-right: 1.5rem;
+  }
+
   & button {
     border-radius: 15px;
     background-color: white;
     border: 1px solid black;
-    padding: 5px 10px;
+    padding: 0 1rem;
+    height: 2rem;
+    font-size: 1rem;
+    font-weight: 600;
+    box-shadow: 0px 0px 3px black;
+    margin-right: 1.25rem;
   }
 
   & button:hover {
     background-color: black;
     color: white;
+  }
+
+  & > img {
+    height: 2.5rem;
+    width: 2.5rem;
+    box-shadow: rgb(0 0 0 / 50%) 0px 0px 8px;
+    border-radius: 50%;
+    object-fit: cover;
+  }
+
+  & svg:last-child {
+    font-size: 1.5rem;
+    margin-left: 0.25rem;
+    fill: rgb(134, 142, 150);
   }
 `;
