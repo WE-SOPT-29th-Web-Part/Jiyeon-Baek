06b0fe7138519ac1de10c3b3fd5854ac582688e8
100Gyeon <bjy9966@naver.com>
2021-11-09 18:29:55 +0900

    feat: Profile 스타일링

--- react-velog/src/components/home/Profile.jsx
+++ react-velog/src/components/home/Profile.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
+import profileImg from '../../assets/images/profile.png';
 
 const Profile = () => {
-  return <div></div>;
+  return (
+    <StyledProfile>
+      <img src={profileImg} alt="profile" />
+      <StyledInfo>
+        <div>100Gyeon</div>
+        <div>한 줄 소개</div>
+      </StyledInfo>
+    </StyledProfile>
+  );
 };
 
 export default Profile;
+
+const StyledProfile = styled.div`
+  display: flex;
+  align-items: center;
+  width: 70%;
+  margin: 0 auto;
+
+  & > img {
+    border-radius: 50%;
+    width: 8rem;
+    height: 8rem;
+  }
+`;
+
+const StyledInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 1rem;
+
+  & > div:first-child {
+    font-size: 1.5rem;
+    line-height: 1.5;
+    font-weight: bold;
+  }
+
+  & > div:last-child {
+    font-size: 1.125rem;
+    line-height: 1.5;
+    margin-top: 0.25rem;
+    color: rgb(73, 80, 87);
+  }
+`;
