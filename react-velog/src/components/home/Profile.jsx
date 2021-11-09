7981127311dc4040b22cbad2d2f1b8b708931985
100Gyeon <bjy9966@naver.com>
2021-11-09 22:28:51 +0900

    feat: hover 되었을 때 svg 색깔 바뀌도록 구현

--- react-velog/src/components/home/Profile.jsx
+++ react-velog/src/components/home/Profile.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 import profileImg from '../../assets/images/profile.png';
+import { ReactComponent as GithubIcon } from '../../assets/icons/github.svg';
+import { ReactComponent as TwitterIcon } from '../../assets/icons/twitter.svg';
+import { ReactComponent as FacebookIcon } from '../../assets/icons/facebook.svg';
+import { ReactComponent as HomeIcon } from '../../assets/icons/home.svg';
+import { ReactComponent as MailIcon } from '../../assets/icons/mail.svg';
 
 const Profile = () => {
   return (
-    <StyledProfile>
-      <img src={profileImg} alt="profile" />
-      <StyledInfo>
-        <div>100Gyeon</div>
-        <div>한 줄 소개</div>
-      </StyledInfo>
-    </StyledProfile>
+    <>
+      <StyledProfile>
+        <img src={profileImg} alt="profile" />
+        <StyledInfo>
+          <div>100Gyeon</div>
+          <div>한 줄 소개</div>
+        </StyledInfo>
+      </StyledProfile>
+      <StyledIconWrapper>
+        <GithubIcon />
+        <TwitterIcon />
+        <FacebookIcon />
+        <HomeIcon />
+        <MailIcon />
+      </StyledIconWrapper>
+    </>
   );
 };
 
@@ -48,5 +62,19 @@ const StyledInfo = styled.div`
     line-height: 1.5;
     margin-top: 0.25rem;
     color: rgb(73, 80, 87);
+  }
+`;
+
+const StyledIconWrapper = styled.div`
+  display: flex;
+
+  & svg {
+    cursor: pointer;
+    fill: rgb(173, 181, 189);
+    margin-right: 1rem;
+  }
+
+  & svg:hover {
+    fill: rgb(77, 77, 77);
   }
 `;
