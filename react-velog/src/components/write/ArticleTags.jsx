8af872f93e7874b6a16c3239170d6afb418b56ad
100Gyeon <bjy9966@naver.com>
2021-11-27 18:07:27 +0900

    feat: tag 제거 기능 구현

--- react-velog/src/components/write/ArticleTags.jsx
+++ react-velog/src/components/write/ArticleTags.jsx
@@ -1,49 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
+import { StyledTag } from 'components/home/ArticleCard';
 
-const ArticleTags = ({ articleData, setArticleData }) => {
+const ArticleTags = ({ articleData, onArrDataChange, onArrDataRemove }) => {
   const tags = articleData.tags;
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
-      // input 내용 -> tag로 추가
-      // 불변성 유지를 위해 아래처럼 작성
-      const tempData = { ...articleData };
-      tempData.tags = [...tempData.tags, e.target.value];
-      setArticleData(tempData);
-
+      onArrDataChange('tags', e.target.value);
       e.target.value = '';
     }
   };
   return (
-    <StyledTag>
-      {tags.map((tag) => (
-        <span key={tag}>{tag}</span>
-      ))}
+    <StyledRoot>
+      <StyledTag>
+        {tags.map((tag) => (
+          <span key={tag} onClick={(e) => onArrDataRemove('tags', tag)}>
+            {tag}
+          </span>
+        ))}
+      </StyledTag>
       <input
         type="text"
         onKeyPress={handleKeyPress}
         placeholder="태그를 입력하세요."
       />
-    </StyledTag>
+    </StyledRoot>
   );
 };
 
 export default ArticleTags;
 
-const StyledTag = styled.div`
+const StyledRoot = styled.div`
   display: flex;
   flex-wrap: wrap;
-  & > span {
-    background: rgb(241, 243, 245);
-    color: rgb(12, 166, 120);
-    padding: 0 1rem;
-    height: 2rem;
-    line-height: 2rem;
-    font-size: 1rem;
-    border-radius: 1rem;
-    margin-right: 0.75rem;
-    margin-bottom: 0.75rem;
-  }
   & > input {
     outline: 0;
     border: 0;
